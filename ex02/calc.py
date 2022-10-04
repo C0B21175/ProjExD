9dc43b3f186b5c2e0bcdd5133377bd48aff38ec7
shota <c0b211756e@edu.teu.ac.jp>
2022-10-04 14:32:50 +0900

    練習２まで

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,10 +1,21 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-root = tk.TK()
+root = tk.Tk()
 root.geometry("300x500")
 
-font=("Times New Roman", 30)
+r=0
+c=0
+for i , num in enumerate(range(9,-1,-1),1):
+    button = tk.Button(root, text=f"{num}",font=("Times New Roman", 30),width=4,height=2)
+    button.grid(row=r, column=c)
+    c += 1
+    if i%3 == 0:
+        r += 1
+        c = 0
+
+
+
 
 
 root.mainloop()
