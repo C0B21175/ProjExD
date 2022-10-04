985cdbc08fbc99781f7ea08bf107eefb894ecb01
shota <c0b211756e@edu.teu.ac.jp>
2022-10-04 15:03:50 +0900

    練習5まで

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,9 +1,12 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+
 def click_number(event):
-    botton =event.widget
-    num =int(button["text"])
-    tkm.showinfo(f"{num}",f"{num}のボタンが押されました")
+    btn =event.widget
+    num =int(btn["text"])
+    #tkm.showinfo(f"{num}",f"{num}のボタンが押されました")
+    entry.insert(tk.END, num)
+
 
 root = tk.Tk()
 root.geometry("300x500")
@@ -16,9 +19,9 @@ entry.grid(row=0,column=0,columnspan=3)
 r=1
 c=0
 for i , num in enumerate(range(9,-1,-1),1):
-    button = tk.Button(root, text=f"{num}",font=("Times New Roman", 30),width=4,height=2)
-    button.bind("<1>",click_number)
-    button.grid(row=r, column=c)
+    btn = tk.Button(root, text=f"{num}",font=("Times New Roman", 30),width=4,height=2)
+    btn.bind("<1>",click_number)
+    btn.grid(row=r, column=c)
     c += 1
     if i%3 == 0:
         r += 1
