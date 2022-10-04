99c32c6cdd3c8148ce7459a00d4318f9d7a44998
shota <c0b211756e@edu.teu.ac.jp>
2022-10-04 15:31:34 +0900

    練習7まで

--- ex02/calc.py
+++ ex02/calc.py
@@ -8,6 +8,14 @@ def click_number(event):
     entry.insert(tk.END, num)
 
 
+def click_equal(event):
+    eqn = entry.get()
+    res = eval(eqn)
+    entry.delete(0,tk.END)
+    entry.insert(tk.END,res)
+
+
+
 root = tk.Tk()
 root.geometry("300x500")
 
@@ -18,7 +26,7 @@ entry.grid(row=0,column=0,columnspan=3)
 
 r,c = 1,0
 numbers = list(range(9,-1,-1))
-operators = ["+","="]
+operators = ["+"]
 
 for i , num in enumerate(numbers+operators,1):
     btn = tk.Button(root, text=f"{num}",font=("Times New Roman", 30),width=4,height=2)
@@ -29,9 +37,8 @@ for i , num in enumerate(numbers+operators,1):
         r += 1
         c = 0
 
-
-
-
-
+btn = tk.Button(root, text=f"=",font=("Times New Roman", 30),width=4,height=2)
+btn.bind("<1>",click_equal)
+btn.grid(row=r,column=c)
 
 root.mainloop()
