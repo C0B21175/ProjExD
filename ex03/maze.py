02bee1258e0f09e30af51f34add5992b415554b5
shota <c0b211756e@edu.teu.ac.jp>
2022-10-18 15:49:50 +0900

    ３限基本機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -6,6 +6,7 @@ import maze_maker as mm
 def key_down(event):
     global key
     key = event.keysym
+
 def key_up(event):
     global key
     key = ""
@@ -20,20 +21,30 @@ def main_proc():
         mx -= 1
     if key =="Right":
         mx += 1
-    cx, cy = mx*100+50 , my*100+50
+    if maze_lst[my][mx] == 0:
+        cx, cy = mx*100+50 , my*100+50
+    else:
+        if key =="Up":
+            my += 1
+        if key =="Down":
+            my -= 1
+        if key =="Left":
+            mx += 1
+        if key =="Right":
+            mx -= 1 
     canv.coords("tori",cx,cy)
     root.after(100,main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
-    
-    cx, cy = 300,400
-    mx , my = 1,1
     root.title("迷えるこうかとん")
     canv = tk.Canvas(width=1500,height=900,bg="black")
     canv.pack()
-    mm.show_maze(canv,mm.make_maze(15,9))
+    maze_lst = mm.make_maze(15,9)
+    mm.show_maze(canv,maze_lst)
     tori = tk.PhotoImage(file="ex03/fig/5.png")
+    mx,my = 1,1
+    cx, cy = mx*100+50,my*100+50
     canv.create_image(cx,cy,image=tori,tag="tori")
     key = ""
     root.bind("<KeyPress>",key_down)
