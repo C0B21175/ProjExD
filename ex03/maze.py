40bb610903a011c20822420513952ec1ec089ea2
shota <c0b211756e@edu.teu.ac.jp>
2022-10-18 15:20:18 +0900

    練習3-11まで

--- ex03/maze.py
+++ ex03/maze.py
@@ -11,30 +11,32 @@ def key_up(event):
     key = ""
 
 def main_proc():
-    global cx, cy 
+    global cx, cy, mx, my
     if key =="Up":
-        cy += 20
+        my -= 1
     if key =="Down":
-        cy -= 20
+        my += 1
     if key =="Left":
-        cx -= 20
+        mx -= 1
     if key =="Right":
-        cx += 20
+        mx += 1
+    cx, cy = mx*100+50 , my*100+50
     canv.coords("tori",cx,cy)
     root.after(100,main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
+    
     cx, cy = 300,400
+    mx , my = 1,1
     root.title("迷えるこうかとん")
     canv = tk.Canvas(width=1500,height=900,bg="black")
     canv.pack()
+    mm.show_maze(canv,mm.make_maze(15,9))
     tori = tk.PhotoImage(file="ex03/fig/5.png")
     canv.create_image(cx,cy,image=tori,tag="tori")
     key = ""
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)
-    
-    mm.show_maze(canv,mm.make_maze(15,9))
     main_proc()
     root.mainloop()
