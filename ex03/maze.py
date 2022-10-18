f2f4fb7d0b2d318311a2659c903990e62c6b269f
shota <c0b211756e@edu.teu.ac.jp>
2022-10-18 17:09:42 +0900

    課題実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,4 +1,5 @@
 from calendar import c
+from ctypes.wintypes import HPALETTE
 import tkinter as tk
 import tkinter.messagebox as tkm
 import maze_maker as mm
@@ -12,7 +13,8 @@ def key_up(event):
     key = ""
 
 def main_proc():
-    global cx, cy, mx, my
+    global cx, cy, mx, my,hp
+    hp = 10
     if key =="Up":
         my -= 1
     if key =="Down":
@@ -25,15 +27,26 @@ def main_proc():
         cx, cy = mx*100+50 , my*100+50
     else:
         if key =="Up":
+            hp -= 1
             my += 1
+            
         if key =="Down":
+            hp -= 1
             my -= 1
         if key =="Left":
+            hp -= 1
             mx += 1
         if key =="Right":
-            mx -= 1 
+            hp -= 1
+            mx -= 1
     canv.coords("tori",cx,cy)
-    root.after(100,main_proc)
+    if cx == 1350 and cy == 750:
+        tkm.showinfo("GREAT","goalに着きました")
+        canv.create_image(cx,cy,image=tori1,tag="tori1")
+    elif hp == 0:
+        tkm.showinfo("BAD!!","hpが0になりました")
+    else:
+        root.after(100,main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -42,7 +55,10 @@ if __name__ == "__main__":
     canv.pack()
     maze_lst = mm.make_maze(15,9)
     mm.show_maze(canv,maze_lst)
+    canv.create_rectangle(200,200,100,100,fill="blue")
+    canv.create_rectangle(1300,700,1400,800,fill="red")
     tori = tk.PhotoImage(file="ex03/fig/5.png")
+    tori1= tk.PhotoImage(file="ex03/fig/9.png")
     mx,my = 1,1
     cx, cy = mx*100+50,my*100+50
     canv.create_image(cx,cy,image=tori,tag="tori")
