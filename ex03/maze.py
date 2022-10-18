c248384f52c76d2a05a93566759abce0ed0c18a5
shota <c0b211756e@edu.teu.ac.jp>
2022-10-18 14:47:07 +0900

    練習３－8まで

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,13 +1,38 @@
+from calendar import c
 import tkinter as tk
 import tkinter.messagebox as tkm
+import maze_maker
+
+def key_down(event):
+    global key
+    key = event.keysym
+def key_up(event):
+    global key
+    key = ""
+
+def main_proc():
+    global cx, cy 
+    if key =="Up":
+        cy += 20
+    if key =="Down":
+        cy -= 20
+    if key =="Left":
+        cx -= 20
+    if key =="Right":
+        cx += 20
+    canv.coords("tori",cx,cy)
+    root.after(100,main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
+    cx, cy = 300,400
     root.title("迷えるこうかとん")
     canv = tk.Canvas(width=1500,height=900,bg="black")
     canv.pack()
     tori = tk.PhotoImage(file="ex03/fig/5.png")
-    cx,cy = 300,400
     canv.create_image(cx,cy,image=tori,tag="tori")
     key = ""
+    root.bind("<KeyPress>",key_down)
+    root.bind("<KeyRelease>",key_up)
+    main_proc()
     root.mainloop()
